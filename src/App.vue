7a903a4bd1c4c0dc50f45b84029da8c78431b23c
SoraMameDante <shino3173@gmail.com>
2019-08-09 11:24:08 +0900

    [init]初回コミット

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>FF14</span>
+        <span class="font-weight-light">総合支援システム (仮称)</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid>
+    <v-layout wrap>
+      <v-flex xs2>
+        <v-card>
+          <v-container>
+            <v-layout>
+              <v-flex xs12>
+                <v-select v-model="selectItem" :items="items"></v-select>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs8>
+        <v-card raised>
+          <v-container>
+            <v-layout>
+              <v-flex xs12>
+                <v-calendar></v-calendar>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: ["user1", "user2", "user3", "user4", "user5"],
+    selectItem: []
+  })
+};
+</script>
